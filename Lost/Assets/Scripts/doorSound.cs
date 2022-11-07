82c344fed89d1e3447f39e0d8e52571cb99a9195
ramukaka99 <101230824@students.swinburne.edu.my>
2022-11-07 16:38:57 +0530

    door test

---
+++ Lost/Assets/Scripts/doorSound.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public bool hasPlayed = false;
+    public GameObject Vase;
+    public GameObject BrokeVase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !audioSource.isPlaying && !hasPlayed)
+        {
+            audioSource.Play();
+            hasPlayed = true;
+
+            Destroy(Vase);
+            Instantiate(BrokeVase);
+        }
+    }
+}
