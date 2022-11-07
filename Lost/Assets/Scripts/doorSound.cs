249c41d44a7b34c433a2013047cce5b16260b08f
ramukaka99 <101230824@students.swinburne.edu.my>
2022-11-07 18:40:24 +0530

    attach torch

--- Lost/Assets/Scripts/doorSound.cs
+++ Lost/Assets/Scripts/doorSound.cs
@@ -29,7 +29,7 @@ public class doorSound : MonoBehaviour
             hasPlayed = true;
 
             Destroy(Vase);
-            Instantiate(BrokeVase);
+            Instantiate(BrokeVase, transform.position, Quaternion.identity);
         }
     }
 }
