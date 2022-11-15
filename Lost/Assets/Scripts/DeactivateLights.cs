1de7de9b329200bdeaf33569c20c0b768c39a7e8
Xenseitional <samuel-lau2000@hotmail.my>
2022-11-16 04:19:27 +0800

    minor changes

--- Lost/Assets/Scripts/DeactivateLights.cs
+++ Lost/Assets/Scripts/DeactivateLights.cs
@@ -7,14 +7,18 @@ public class DeactivateLights : MonoBehaviour
     public AudioSource audioSource;
     public GameObject objectDeactivate;
     public bool hasPlayed = false;
+    public GameObject cricketSound;
+    public GameObject newCricketSound;
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !audioSource.isPlaying && !hasPlayed)
         {
             audioSource.Play();
             hasPlayed = true;
             objectDeactivate.SetActive(false);
+            Destroy(cricketSound);
+            newCricketSound.SetActive(true);
         }
     }
 }
--- Lost/Assets/Scripts/KeyMech.cs
+++ Lost/Assets/Scripts/KeyMech.cs
@@ -6,11 +6,13 @@ public class KeyMech : MonoBehaviour
 {
     public GameObject lockBar;
     public GameObject key;
+    public AudioSource unlocked;
 
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.CompareTag("Key"))
         {
+            unlocked.Play();
             Destroy(lockBar);
             Destroy(key);
             Debug.Log("Key to door");
