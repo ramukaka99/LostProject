b60078d7e4e353b8ff03c87cccde45fc744309e5
ramukaka99 <101230824@students.swinburne.edu.my>
2022-11-16 22:57:44 +0530

    update on subs

--- Lost/Assets/Scripts/Subtitle.cs
+++ Lost/Assets/Scripts/Subtitle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Subtitle : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class Subtitle : MonoBehaviour
     //public GameObject uiObject;
     public GameObject dialogue;
     public bool hasPlayed = false;
+    [SerializeField] private float duration;
+    [SerializeField] int letterPerSeconds;
+    [SerializeField] TextMeshProUGUI dialogText;
+    [SerializeField] string Text;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +29,25 @@ public class Subtitle : MonoBehaviour
             hasPlayed = true;
             //uiObject.SetActive(true);
             dialogue.SetActive(true);
+            StartCoroutine(TypeDialog(Text));
             StartCoroutine("WaitForSec");
         }
     }
 
+    IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach(var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / letterPerSeconds);
+        }
+    }
+
+
     IEnumerator WaitForSec()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(duration);
         Destroy(dialogue);
     }
 }
