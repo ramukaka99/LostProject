b03b12a607e01304eed427a26867911bbd82daba
ramukaka99 <101230824@students.swinburne.edu.my>
2022-11-30 21:35:17 +0800

    keypad

--- Lost/Assets/Scripts/KeypadCont.cs
+++ Lost/Assets/Scripts/KeypadCont.cs
@@ -21,5 +21,77 @@ public class KeypadCont : MonoBehaviour
 
     public bool HasUsedCorrectCode { get { return hasUsedCorrectCode; } }
 
+    public void UserNumberEntry(int selectedNum)
+    {
+        if (inputPasswordList.Count >= 4)
+            return;
 
+        inputPasswordList.Add(selectedNum);
+
+        UpdateDisplay();
+
+        if (inputPasswordList.Count >= 4)
+            CheckPassword();
+    }
+
+    private void CheckPassword()
+    {
+        for (int i = 0; i < correctPassword.Count; i++)
+        {
+            if (inputPasswordList[i] != correctPassword[i])
+            {
+                IncorrectPassword();
+                return;
+            }
+        }
+        correctPasswordGiven();
+    }
+
+    private void correctPasswordGiven()
+    {
+        if (allowMultipleAct)
+        {
+            OnIncorrectPassword.Invoke();
+            StartCoroutine(ResetKeycode());
+        }
+        else if( !allowMultipleAct && !hasUsedCorrectCode)
+        {
+            onCorrectPassword.Invoke();
+            hasUsedCorrectCode = true;
+            codeDisplay.text = successText;
+        }
+    }
+
+    private void IncorrectPassword()
+    {
+        OnIncorrectPassword.Invoke();
+        StartCoroutine(ResetKeycode());
+    }
+
+    private void UpdateDisplay()
+    {
+        codeDisplay.text = null;
+        for(int i = 0; i < inputPasswordList.Count; i++)
+        {
+            codeDisplay.text += inputPasswordList[i];
+        }
+    }
+
+    public void DeleteEntry()
+    {
+        if (inputPasswordList.Count <= 0)
+            return;
+        var listposition = inputPasswordList.Count - 1;
+        inputPasswordList.RemoveAt(listposition);
+
+        UpdateDisplay();
+    }
+
+    IEnumerator ResetKeycode()
+    {
+        yield return new WaitForSeconds(resetTime);
+
+        inputPasswordList.Clear();
+        codeDisplay.text = "Enter Code...";
+    }
 }
